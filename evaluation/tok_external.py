6c3de054254f0f83f80b339bd27d9cc71afadd8e
Oleg Parashchenko <olpa@uucode.com>
2022-07-20 07:09:19 +0200

    Re-run an external on each batch run.

--- evaluation/tok_external.py
+++ evaluation/tok_external.py
@@ -15,21 +15,28 @@ MOSES_EN_COMMAND = [
 JBOPARSE_COMMAND = 'jboparse.py'
 
 
-def get_external_tokenizer(command):
-    return subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+class ExternalTokenizer:
+    def __init__(self, command):
+        self.command = command
+
+    def execute(self, text):
+        pipe = subprocess.Popen(self.command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        back = pipe.communicate(text)
+        if back[1]:
+            raise SystemError(f'stderr from the tokenizer: {back[1]}')
+        return back[0]
+
 
 def get_moses_en_tokenizer():
-    return get_external_tokenizer(MOSES_EN_COMMAND)
+    return ExternalTokenizer(MOSES_EN_COMMAND)
 
 def get_jbo_tokenizer():
-    return get_external_tokenizer(JBOPARSE_COMMAND)
+    return ExternalTokenizer(JBOPARSE_COMMAND)
 
 def batch_tokenize(tokenizer, sentences):
     text = '\n'.join(sentences).encode('utf-8')
-    back = tokenizer.communicate(text)
-    if back[1]:
-        raise SystemError(f'stderr from the tokenizer: {back[1]}')
-    tokenized = str(back[0], 'utf-8').split('\n')
+    back = tokenizer.execute(text)
+    tokenized = str(back, 'utf-8').split('\n')
     if tokenized and tokenized[-1] == '':
         tokenized.pop()
     assert len(sentences) == len(tokenized), f'Number of input sentences: {len(sentences)}, number of tokenized sentences: {len(tokenized)}'
@@ -37,8 +44,8 @@ def batch_tokenize(tokenizer, sentences):
 
 
 if '__main__' == __name__:
-    tokenizer = get_moses_en_tokenizer()
-    #tokenizer = get_jbo_tokenizer()
+    #tokenizer = get_moses_en_tokenizer()
+    tokenizer = get_jbo_tokenizer()
     text = [li.strip() for li in sys.stdin]
     back = batch_tokenize(tokenizer, text)
     print('\n'.join(back))
