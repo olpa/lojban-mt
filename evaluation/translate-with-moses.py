db625f3bb06bc02ff08837b79ab9d872d81a8535
Oleg Parashchenko <olpa@uucode.com>
2022-07-22 06:37:43 +0200

    Import translation code from zmifanva.

--- evaluation/translate-with-moses.py
+++ evaluation/translate-with-moses.py
@@ -1,5 +1,6 @@
 import argparse
 
+# The core part is mostly a copy/paste from zmifanva/web.
 
 def parse_command_line():
     parser = argparse.ArgumentParser()
@@ -10,25 +11,56 @@ def parse_command_line():
     parser.add_argument('--tgt-field', required=True)
     return parser.parse_args()
 
+PUNC_ENTITY_MAP = [
+    # ('&', '&amp;'),  # handle specially
+    ('|', '&#124;'),
+    ('<', '&lt;'),
+    ('>', '&gt;'),
+    ('\'', '&apos;'),
+    ('"', '&quot;'),
+    ('[', '&#91;'),
+    (']', '&#93;')
+]
 
-def translate(moses_server, sentence):
+
+def escape_html_entities(text):
+    text = text.replace('&', '&amp;')
+    for punc, entity in PUNC_ENTITY_MAP:
+        text = text.replace(punc, entity)
+    return text
 
 
-        # English to Lojban translation
-        src = ' '.join(tokenize_en(src))
-        src = escape_html_entities(src)
-        try:
-            tr = moses_server.translate({'text': src})
-            tgt = tr['text']
-        except Exception as e:
-            return { 'tgt': '', 'error': 'Error calling moses: ' + str(e) }
-        tgt = unescape_html_entities(tgt)
-        return { 'tgt': tgt }
-    else:
+def unescape_html_entities(text):
+    for punc, entity in PUNC_ENTITY_MAP:
+        text = text.replace(entity, punc)
+    text = text.replace('&amp;', '&')
+    return text
+
+
+def translate_sentence(moses_server, sentence):
+    src = escape_html_entities(sentence)
+    tr = moses_server.translate({'text': src})
+    tgt = tr['text']
+    tgt = unescape_html_entities(tgt)
+    return tgt
+
+
+def translate(ds, moses_server, src_fields, tgt_field):
+    new_splits = {}
+    for name, split in ds.items():
+        sentences = split[src_field]
+        translations = [
+                translate_sentence(moses_server, sentence)
+                for sentence in sentences]
+        new_splits[name] = split.add_column(tgt_field, translations)
+    for name, split in new_splits.items():
+        ds[name] = split
 
 def main():
     args = parse_command_line()
-    print(args)
-
+    moses_server = xmlrpc.client.ServerProxy(args.endpoint)
+    ds = datasets.load_from_disk(args.dataset)
+    translate(ds, moses_server, args.src_field, args.tgt_field)
+    ds.save_to_disk(args.output)
 
 main()
